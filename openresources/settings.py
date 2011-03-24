3ea9d757bc25e794a9f43b2e09461fa2692478d0
floledermann <ledermann@ims.tuwien.ac.at>
2011-03-24 10:38:04 +0000

    customizable map data attribution

--- openresources/settings.py
+++ openresources/settings.py
@@ -1,4 +1,7 @@
 
 from django.conf import settings
 
-TAG_HELP_LINKS = getattr(settings, 'RESOURCES_TAG_HELP_LINKS', None)
+TAG_HELP_LINKS = getattr(settings, 'OPENRESOURCES_TAG_HELP_LINKS', None)
+
+MAP_ATTRIBUTION = getattr(settings, 'OPENRESOURCES_MAP_ATTRIBUTION', 'Resource Data collected with <a href="http://bitbucket.org/floledermann/openresources/">OpenResources</a>')
+
