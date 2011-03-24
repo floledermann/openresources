c167d294a2a36da4cd5d2976072120c39a49ea9a
floledermann <ledermann@ims.tuwien.ac.at>
2011-03-24 10:39:17 +0000

    including user actions in base template, showing login/logout and language switcher if available

--- testproject/urls.py
+++ testproject/urls.py
@@ -3,6 +3,8 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 
+from django.contrib.auth import views as auth_views
+
 import os
 
 from django.contrib import admin
@@ -13,5 +15,8 @@ urlpatterns = patterns('',
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', auth_views.login, {'template_name': 'auth/login.html'}, name='auth_login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'auth/logout.html'}, name='auth_logout'),
+    (r'^lang/', include('django.conf.urls.i18n')),
     (r'^', include('openresources.urls')),
 )
