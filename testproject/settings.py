e3e870a7322ce62d6bed55831ab80baaff89e8fa
floledermann <ledermann@ims.tuwien.ac.at>
2011-04-18 15:41:58 +0100

    add LocaleMiddleware to testproject settings to make i18n work

--- testproject/settings.py
+++ testproject/settings.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # Django settings for openresources test project.
 # Requires Django >= 1.2
 
@@ -23,6 +25,7 @@ USE_L10N = True
 LANGUAGES = (
     ('en', 'English'),
     ('de', 'Deutsch'),
+    ('fr', 'Français'),
 )
 
 MEDIA_ROOT = ''
@@ -42,6 +45,7 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
