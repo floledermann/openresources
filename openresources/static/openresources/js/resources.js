345031588e3ce34f32a13509f72e7f645f40d0c1
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2012-11-08 21:52:23 +0100

    tweaking index page

--- openresources/static/openresources/js/resources.js
+++ openresources/static/openresources/js/resources.js
@@ -10,4 +10,20 @@ jQuery(function($) {
         $this.toggleClass('on');
         $this.toggleClass('off');
     });
+
+    function updateLocation(position) {
+      // Show a map centered at (position.coords.latitude, position.coords.longitude).
+        alert(position.coords.latitude);
+    }
+    if (navigator.geolocation) {
+        $('.uselocationbutton').click(function(){
+            navigator.geolocation.getCurrentPosition(updateLocation, function(error) {
+                alert("Error getting position: " + error.message);            
+            });
+        });
+    }
+    else {
+        $('.uselocationbutton').addClass('disabled');
+        $('.uselocationbutton').attr('disabled','disabled');
+    }
 });
